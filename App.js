7da44a477ce6d4bb9dc3907485e4ea1a3555fb44
Filippo Barcellos <barcellos.filippo@gmail.com>
2022-04-29 20:23:40 +0100

    fix

--- App.js
+++ App.js
@@ -1,41 +1,23 @@
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { View, StyleSheet, Text } from 'react-native';
-import { useRef } from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { View, StyleSheet, Text } from 'react-native'
+import { useRef } from 'react'
 
-import BottomSheet1 from './components/BottomSheet/index';
-import GoalSheet from './components/BottomSheet/GoalSheet';
+import BottomSheet1 from './components/Option2/index'
+import GoalSheet from './components/Option2/GoalSheet'
 
 // import ChatSheet from './components/Option1/index';
 // import GoalSheet from './components/Option1/index2';
 // import PreferencesSheet from './components/Option1/Preferences';
 
 export default function App() {
-  const chatSheetRef = useRef(null);
-  const goalSheetRef = useRef(null);
-  const preferencesSheetRef = useRef(null);
-
-  // const onGoalSheetOpen = () => {
-  //   goalSheetRef.current.expand();
-  //   chatSheetRef.current.close();
-  // };
-
-  // const onGoalSheetClose = () => {
-  //   goalSheetRef.current.collapse();
-  //   chatSheetRef.current.snapToIndex(1);
-  // };
-
-  // const onPreferencesOpen = () => {
-  //   preferencesSheetRef.current.expand();
-  // };
-
-  // const onPreferencesClose = () => {
-  //   preferencesSheetRef.current.close();
-  // };
+  const chatSheetRef = useRef(null)
+  const goalSheetRef = useRef(null)
+  const preferencesSheetRef = useRef(null)
 
   const onGoalSheetOpen = () => {
-    goalSheetRef.current.expand();
-  };
+    goalSheetRef.current.expand()
+  }
 
   return (
     <SafeAreaProvider>
@@ -44,19 +26,14 @@ export default function App() {
           <Text style={styles.title}>App</Text>
         </View>
       </View>
-      <GoalSheet
-        ref={goalSheetRef}
-        onExpand={onGoalSheetOpen}
-        // onOpen={onGoalSheetOpen}
-        // onClose={onGoalSheetClose}
-      />
+      <GoalSheet ref={goalSheetRef} onExpand={onGoalSheetOpen} />
       <BottomSheet1 ref={chatSheetRef} />
       {/* <PreferencesSheet
           ref={preferencesSheetRef}
           onClose={onPreferencesClose}
         /> */}
     </SafeAreaProvider>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -78,4 +55,4 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     paddingTop: 20,
   },
-});
+})
